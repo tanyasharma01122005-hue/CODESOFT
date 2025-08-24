5e976f2215a5a6b66e39316600fe28cf58333623
tanyasharma01122005-hue <tanyasharma.01122005@gmail.com>
2025-08-24 19:48:27 +0530

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -4,7 +4,7 @@ def calculator():
 # Now take input from the user
   number_1=float(input("Enter first number:-")
   number_2=float(input("Enter second number (in case of division not equals to zero):-")
-  chooose_operation=input("choose from(+,-,/,*)")
+  choose_operation=input("choose from(+,-,/,*):-")
   # Here if and elif and else statement is used 
   if choose_operation=='+':
     result = number_1 + number_2
